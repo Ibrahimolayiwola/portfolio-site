cbc5bf57927bd7a5454a3cf6065ce3f708721ee6
Highbee <ibrahimolayiwola333@gmail.com>
2025-02-17 01:30:22 +0100

    completed the portfolio UI

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,44 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          cssFile: '',
+        }
+      },
+      transformIndexHtml: (html) => {
+        const cssFile = findHashedCssFile();
+        return html.replace('<%= cssFile %>', cssFile);
+      },
+    }),
+  ],
+
   server: {
     herders: {
       'Cache-Control': 'public, max-age=3600'
     }
+  },
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+    manifest: true,
   }
 })
+
+function findHashedCssFile() {
+  const distPath = path.resolve(__dirname, 'dist/assets');
+  const files = fs.readdirSync(distPath);
+  const cssFile = files.find(file => file.endsWith('.css'));
+  return cssFile ? `/assets/${cssFile}` : '';
+}

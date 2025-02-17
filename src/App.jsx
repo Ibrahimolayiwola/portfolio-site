cb638cc0d864ee8b1f81b132fd3c01a771f183cf
Highbee <ibrahimolayiwola333@gmail.com>
2025-02-17 01:53:20 +0100

    fix production bug in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Header from './layouts/header/Header.jsx'
-import Main from './layouts/Main/Main.jsx'
+import Main from './layouts/main/Main.jsx'
 import Footer from './layouts/footer/Footer.jsx'
 
 
